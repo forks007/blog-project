70de15138c0850fb8645da7a7fd3224a6ca1a019
fahmed <u1102014@student.cuet.ac.bd>
2021-11-08 01:07:29 +0600

    created the index demo page

---
+++ script.js
@@ -0,0 +1,130 @@
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll',fixNav)
+
+function fixNav() {
+    if(scrollY > nav.offsetHeight +50){
+        nav.classList.add('active')
+    }else{
+        nav.classList.remove('active')
+    }
+}
+
+
+
+const carousel = document.querySelector(".carousel")
+const slideContainer = document.getElementById('slide-container')
+const slides = slideContainer.querySelectorAll('.slide')
+
+console.log(slides.length)
+
+const left = document.getElementById('left')
+const right = document.getElementById('right')
+
+
+const links = ["https://source.unsplash.com/user/erondu/1600x900","https://source.unsplash.com/collection/190727/1600x900","https://source.unsplash.com/collection/190726/1600x900","https://source.unsplash.com/collection/190725/1600x900"]
+
+
+let idx = 0;
+let interval = setInterval(run, 2000);
+function run(){
+    idx++
+    changeSlide()
+}
+
+function changeSlide(){
+    if(idx>slides.length-1) {idx=0}
+    else if(idx<0){idx=slides.length-1}
+    slideContainer.style.transform = `translateX(${idx*-150}%)`
+    
+    // setActiveSlide()
+    setBgToCarousel(idx)
+}
+function setActiveSlide(){
+    slides.forEach((slide)=>slide.classList.remove('active'))
+
+    slides[idx].classList.add('active')
+    
+}
+
+function setBgToCarousel(idx){
+    carousel.style.backgroundImage = `url(${links[idx]})`
+}
+
+function resetInterval(){
+    clearInterval(interval)
+    interval = setInterval(run, 2000);
+}
+
+
+
+
+
+
+
+
+
+
+left.addEventListener('click',()=>{
+    idx--
+    changeImage()
+    resetInterval()
+})
+right.addEventListener('click',()=>{
+    idx++
+    changeImage()
+    resetInterval()
+})
+
+
+
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        const x = e.x
+        const y = e.y
+
+        const btnTop = e.target.offsetTop;
+        const btnLeft = e.target.offsetLeft;
+        
+
+        const circleY = y - btnTop;
+        const circleX= x- btnLeft;
+        // console.log(circleX,circleY);
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = `${circleY}px`
+        circle.style.left = `${circleX}px`
+
+        button.appendChild(circle)
+
+        setTimeout(()=>{
+            circle.remove()
+        },1000)
+    
+    })
+
+  
+})
+
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter=>{
+    counter.innerText = '0'
+    const updateCounter = ()=>{
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target/200
+
+        if(c<target){
+            counter.innerText = `${Math.ceil(c+increment)}`
+            setTimeout(updateCounter,1)
+        }else{
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+})
